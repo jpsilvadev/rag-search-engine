90cc496357471b19c4533e09d00e9ea75d517e6e
jpsilvadev <jpsjoaosilva98@gmail.com>
2026-08-19 16:02:01 +0100

    search through built index instead of raw data

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -19,17 +19,8 @@ class InvertedIndex:
     def __init__(self) -> None:
         self.index: dict[str, set[int]] = {}
         self.docmap: dict[int, Movie] = {}
-
-    def __add_document(self, doc_id: int, text: str) -> None:
-        tokens = tokenize(text)
-        for token in tokens:
-            if token not in self.index:
-                self.index[token] = set()
-            self.index[token].add(doc_id)
-
-    def get_documents(self, term: str) -> list[int]:
-        term_doc_ids = self.index.get(term, set())
-        return sorted(term_doc_ids)
+        self.index_path = os.path.join(CACHE_PATH, "index.pkl")
+        self.docmap_path = os.path.join(CACHE_PATH, "docmap.pkl")
 
     def build(self):
         movies = load_movies()
@@ -40,24 +31,46 @@ class InvertedIndex:
     def save(self) -> None:
         if not os.path.exists(CACHE_PATH):
             os.makedirs(CACHE_PATH)
-        with open(os.path.join(CACHE_PATH, "index.pkl"), "wb") as handler:
+        with open(self.index_path, "wb") as handler:
             pickle.dump(self.index, handler)
 
-        with open(os.path.join(CACHE_PATH, "docmap.pkl"), "wb") as handler:
+        with open(self.docmap_path, "wb") as handler:
             pickle.dump(self.docmap, handler)
+
+    def load(self) -> None:
+        with open(self.index_path, "rb") as handler:
+            self.index = pickle.load(handler)
+        with open(self.docmap_path, "rb") as handler:
+            self.docmap = pickle.load(handler)
+
+    def get_documents(self, term: str) -> list[int]:
+        term_doc_ids = self.index.get(term, set())
+        return sorted(term_doc_ids)
+
+    def __add_document(self, doc_id: int, text: str) -> None:
+        tokens = tokenize(text)
+        for token in tokens:
+            if token not in self.index:
+                self.index[token] = set()
+            self.index[token].add(doc_id)
 
 
 def search_command(query, limit: int = DEFAULT_SEARCH_LIMIT) -> list[Movie]:
-    movies = load_movies()
-    results = []
-
+    index = InvertedIndex()
+    index.load()
     query_tokens = tokenize(query)
 
-    for movie in movies:
-        title_tokens = tokenize(movie["title"])
-        # match at least one query token with any title token
-        if has_matching_token(query_tokens, title_tokens):
-            results.append(movie)
+    seen = set()
+    results = []
+    for token in query_tokens:
+        matching_doc_ids = index.get_documents(token)
+
+        for doc_id in matching_doc_ids:
+            # avoid unnecessary lookups
+            if doc_id in seen:
+                continue
+            seen.add(doc_id)
+            results.append(index.docmap.get(doc_id))
             if len(results) >= limit:
                 break
     return results
@@ -67,10 +80,6 @@ def build_command() -> None:
     index = InvertedIndex()
     index.build()
     index.save()
-
-    # hardcoded for testing purposes
-    docs = index.get_documents("merida")
-    print(f"First document for token 'merida' = {docs[0]}")
 
 
 def tokenize(text: str) -> list[str]:
@@ -87,9 +96,3 @@ def tokenize(text: str) -> list[str]:
     # stemming
     stemmed_words = list(map(stemmer.stem, filtered_tokens))
     return stemmed_words
-
-
-def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
-    return any(
-        q_token in t_token for q_token in query_tokens for t_token in title_tokens
-    )
