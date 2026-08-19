8448ea0fdf7e22793542be0abe40b1f29563c009
jpsilvadev <jpsjoaosilva98@gmail.com>
2026-08-19 12:59:40 +0100

    filter stop words and add stemming

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,36 +1,46 @@
-import string
+from nltk.stem import PorterStemmer
 
-from .search_utils import DEFAULT_SEARCH_LIMIT, Movie, load_movies
+from .search_utils import (
+    DEFAULT_SEARCH_LIMIT,
+    Movie,
+    load_movies,
+    load_stop_words,
+    preprocess_text,
+)
+
+STOP_WORDS = load_stop_words()
 
 
 def search_command(query, limit: int = DEFAULT_SEARCH_LIMIT) -> list[Movie]:
     movies = load_movies()
     results = []
-    query = tokenize(query)
+
+    query_tokens = tokenize(query)
 
     for movie in movies:
         title_tokens = tokenize(movie["title"])
         # match at least one query token with any title token
-        if has_matching_token(query, title_tokens):
+        if has_matching_token(query_tokens, title_tokens):
             results.append(movie)
             if len(results) >= limit:
                 break
     return results
 
 
-def preprocess_text(text: str) -> str:
-    # case sensitivity
-    text = text.lower()
-    # remove punctuation
-    text = text.translate(str.maketrans("", "", string.punctuation))
-    return text
-
-
 def tokenize(text: str) -> list[str]:
     text = preprocess_text(text)
     tokens = text.split()
     # tokenization: word-based
-    return [token.strip() for token in tokens if token]
+    valid_tokens = [token.strip() for token in tokens if token]
+
+    # filter out stop words
+    filtered_tokens = [token for token in valid_tokens if token not in STOP_WORDS]
+
+    stemmer = PorterStemmer()
+
+    # stemming
+    stemmed_words = list(map(stemmer.stem, filtered_tokens))
+    return stemmed_words
 
 
 def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+import string
 from typing import TypedDict
 
 
@@ -14,10 +15,25 @@ DEFAULT_SEARCH_LIMIT = 5
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
 
 
 # helpers
+def preprocess_text(text: str) -> str:
+    # case sensitivity
+    text = text.lower()
+    # remove punctuation
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
+
+
 def load_movies() -> list[Movie]:
     with open(DATA_PATH, "r", encoding="utf-8") as f:
         data = json.load(f)
         return data["movies"]
+
+
+def load_stop_words() -> list[str]:
+    with open(STOPWORDS_PATH, "r", encoding="utf-8") as f:
+        stop_words = [preprocess_text(word) for word in f.read().splitlines()]
+        return stop_words
